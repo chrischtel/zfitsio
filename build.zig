555cb439300fd5cb683f13cef42dc10791c9fd57
chrischtel <92454917+chrischtel@users.noreply.github.com>
2024-10-30 00:28:05 +0100

    Update build.zig

--- build.zig
+++ build.zig
@@ -34,7 +34,7 @@ pub fn build(b: *std.Build) void {
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
     b.installArtifact(lib);
-
+    _ = b.addModule("zfitsio", .{ .root_source_file = b.path("src/root.zig"), });
     const exe = b.addExecutable(.{
         .name = "zfitsio",
         .root_source_file = b.path("src/main.zig"),
